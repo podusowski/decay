9923acadfff01eb7a7d9c0ad4d681d54de2e5e34
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2021-03-16 22:08:42 +0100

    added Earth

--- src/ephemeris.rs
+++ src/ephemeris.rs
@@ -52,8 +52,21 @@ impl Space {
             name: "Venus",
         });
 
-        // X = 1.973338103014433E-01 Y =-7.001287841606206E-01 Z =-2.098736267890693E-02
-        //VX= 1.933209186041313E-02 VY= 5.418163683984627E-03 VZ=-1.041291312991296E-03
+
+        space.bodies.push(Body {
+            position: Vector {
+                x: Distance::from_aus(9.255531850624332E-01).as_meters(),
+                y: Distance::from_aus(3.708906496672169E-01).as_meters(),
+                z: Distance::from_aus(-1.740719250795340E-05).as_meters(),
+            },
+            velocity: Vector {
+                x: Distance::from_aus(-6.678671400247983E-03).as_meters() / SECONDS_IN_DAY,
+                y: Distance::from_aus(1.589848110988471E-02).as_meters() / SECONDS_IN_DAY,
+                z: Distance::from_aus(5.510397457798760E-08).as_meters() / SECONDS_IN_DAY,
+            },
+            mass: Mass::from_kgs(5.97219e24),
+            name: "Earth",
+        });
 
         space
     }
