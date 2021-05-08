26daf417ec34db4d136639dcb57b4d5bc4e8a0a1
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2021-05-08 15:36:39 +0200

    observer struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate piston_window;
-use piston_window::math::translate;
 use piston_window::*;
+use piston_window::{math::translate, types::Matrix2d};
 
 mod algebra;
 mod ephemeris;
@@ -8,6 +8,26 @@ mod physics;
 mod units;
 
 use physics::*;
+
+struct Observer {
+    view_transform: Matrix2d<f64>,
+    au_as_pixels: f64,
+}
+
+impl Observer {
+    fn zoom_in_out(&mut self, amount: f64) {
+        self.au_as_pixels = (self.au_as_pixels + amount).max(1.0);
+    }
+}
+
+impl Default for Observer {
+    fn default() -> Self {
+        Observer {
+            view_transform: translate([400.0, 400.0]),
+            au_as_pixels: 20.0,
+        }
+    }
+}
 
 fn main() {
     let mut space = Space::solar_system();
@@ -20,26 +40,26 @@ fn main() {
 
     let mut glyphs = window.load_font("./FiraSans-Regular.ttf").unwrap();
 
-    let view_transform = translate([400.0, 400.0]);
-    let mut au_as_pixels = 20.0;
+    let mut observer = Observer::default();
     let the_big_bang_instant = space.time;
 
     while let Some(event) = window.next() {
         if let Event::Input(Input::Move(Motion::MouseScroll(zoom_amount)), _) = event {
-            println!("{:?}", zoom_amount);
-            au_as_pixels = (au_as_pixels + zoom_amount[1]).max(1.0);
+            observer.zoom_in_out(zoom_amount[1]);
         };
 
         window.draw_2d(&event, |context, graphics, device| {
             clear([0.0; 4], graphics);
             for body in &space.bodies {
-                let x = units::Distance::from_meters(body.position.x).as_au() * au_as_pixels;
-                let y = units::Distance::from_meters(body.position.y).as_au() * au_as_pixels;
+                let x =
+                    units::Distance::from_meters(body.position.x).as_au() * observer.au_as_pixels;
+                let y =
+                    units::Distance::from_meters(body.position.y).as_au() * observer.au_as_pixels;
 
                 ellipse(
                     [1.0; 4],
                     [x, y, 10.0, 10.0],
-                    context.transform.append_transform(view_transform),
+                    context.transform.append_transform(observer.view_transform),
                     graphics,
                 );
 
@@ -51,7 +71,7 @@ fn main() {
                     context
                         .transform
                         .trans(x + 10.0, y)
-                        .append_transform(view_transform),
+                        .append_transform(observer.view_transform),
                     graphics,
                 )
                 .unwrap();
