9d8afcb163422e1705a54cb6d1f3a72619a9af29
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2021-07-31 22:21:55 +0200

    chop piston out

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,14 +1,10 @@
-use piston_window::math::translate;
-use piston_window::types::Matrix2d;
-use piston_window::*;
-use piston_window::{text, Context, G2d, Glyphs};
 
 use crate::algebra::{self, Vector};
 use crate::physics::{Body, MassObject, Ship, Space};
 use crate::units;
 
 pub struct Observer {
-    pub view_transform: Matrix2d<f64>,
+    //pub view_transform: Matrix2d<f64>,
 
     /// How many pixels has one astronomical unit.
     au_as_pixels: f64,
@@ -25,7 +21,7 @@ const SYSTEM_WIDE_ZOOM: f64 = 20.0;
 impl Default for Observer {
     fn default() -> Self {
         Observer {
-            view_transform: Default::default(),
+            //view_transform: Default::default(),
             au_as_pixels: SYSTEM_WIDE_ZOOM,
             mouse_cursor: Default::default(),
             selected_body: 0,
@@ -48,8 +44,8 @@ impl Observer {
     }
 
     pub fn look_at(&mut self, position: algebra::Vector) {
-        let (x, y) = self.to_screen_coords(position);
-        self.view_transform = translate([(1280.0 / 2.0) - x, (720.0 / 2.0) - y]);
+        //let (x, y) = self.to_screen_coords(position);
+        //self.view_transform = translate([(1280.0 / 2.0) - x, (720.0 / 2.0) - y]);
     }
 
     pub fn to_screen_coords(&self, position: algebra::Vector) -> (f64, f64) {
@@ -60,30 +56,27 @@ impl Observer {
     }
 
     pub fn to_world_coords(&self, position: (f64, f64)) -> Vector {
-        println!("{:?}", self.view_transform);
-        let x_offset = self.view_transform[0][2];
-        let y_offset = self.view_transform[1][2];
-        Vector {
-            x: units::Distance::from_aus((position.0 - x_offset) / self.au_as_pixels).as_meters(),
-            y: units::Distance::from_aus((position.1 - y_offset) / self.au_as_pixels).as_meters(),
-            z: Default::default(),
-        }
+        //println!("{:?}", self.view_transform);
+        //let x_offset = self.view_transform[0][2];
+        //let y_offset = self.view_transform[1][2];
+        //Vector {
+        //    x: units::Distance::from_aus((position.0 - x_offset) / self.au_as_pixels).as_meters(),
+        //    y: units::Distance::from_aus((position.1 - y_offset) / self.au_as_pixels).as_meters(),
+        //    z: Default::default(),
+        //}
+        Default::default()
     }
 }
 
 /// Draws a single frame. Didn't call it `Graphics` to avoid ambiguity with Piston types.
-pub struct Frame<'a, 'b> {
+pub struct Frame<'a> {
     pub space: &'a Space,
     pub observer: &'a Observer,
-    pub context: &'a Context,
-    pub graphics: &'a mut G2d<'b>,
-    pub device: &'a mut gfx_device_gl::Device,
-    pub glyphs: &'a mut Glyphs,
 }
 
-impl<'a, 'b> Frame<'a, 'b> {
+impl<'a> Frame<'a> {
     pub fn draw(&mut self) {
-        clear([0.0; 4], self.graphics);
+        //clear([0.0; 4], self.graphics);
 
         for body in &self.space.bodies {
             self.draw_body(body);
@@ -95,91 +88,91 @@ impl<'a, 'b> Frame<'a, 'b> {
 
         self.draw_statusbar();
 
-        self.glyphs.factory.encoder.flush(self.device);
+        //self.glyphs.factory.encoder.flush(self.device);
     }
 
     fn draw_label(&mut self, name: &str, position: Vector) {
-        let (x, y) = self.observer.to_screen_coords(position);
+        //let (x, y) = self.observer.to_screen_coords(position);
 
-        text(
-            [0.7; 4],
-            10,
-            name,
-            self.glyphs,
-            self.context
-                .transform
-                .trans(x + 10.0, y)
-                .append_transform(self.observer.view_transform),
-            self.graphics,
-        )
-        .unwrap();
+        //text(
+        //    [0.7; 4],
+        //    10,
+        //    name,
+        //    self.glyphs,
+        //    self.context
+        //        .transform
+        //        .trans(x + 10.0, y)
+        //        .append_transform(self.observer.view_transform),
+        //    self.graphics,
+        //)
+        //.unwrap();
     }
 
     fn draw_body(&mut self, body: &Body) {
-        let (x, y) = self.observer.to_screen_coords(body.position);
+        //let (x, y) = self.observer.to_screen_coords(body.position);
 
-        ellipse(
-            [1.0; 4],
-            [x, y, 10.0, 10.0],
-            self.context
-                .transform
-                .append_transform(self.observer.view_transform),
-            self.graphics,
-        );
+        //ellipse(
+        //    [1.0; 4],
+        //    [x, y, 10.0, 10.0],
+        //    self.context
+        //        .transform
+        //        .append_transform(self.observer.view_transform),
+        //    self.graphics,
+        //);
 
         self.draw_label(body.name, body.position);
     }
 
     fn draw_ship(&mut self, ship: &Ship) {
-        let (x, y) = self.observer.to_screen_coords(ship.position);
+        //let (x, y) = self.observer.to_screen_coords(ship.position);
 
-        ellipse(
-            [1.0; 4],
-            [x, y, 10.0, 10.0],
-            self.context
-                .transform
-                .append_transform(self.observer.view_transform),
-            self.graphics,
-        );
+        //ellipse(
+        //    [1.0; 4],
+        //    [x, y, 10.0, 10.0],
+        //    self.context
+        //        .transform
+        //        .append_transform(self.observer.view_transform),
+        //    self.graphics,
+        //);
 
         self.draw_label(ship.name, ship.position);
     }
 
     fn draw_statusbar(&mut self) {
-        text(
-            [0.7; 4],
-            16,
-            format!("{}", self.space.time).as_str(),
-            self.glyphs,
-            self.context.transform.trans(10.0, 20.0),
-            self.graphics,
-        )
-        .unwrap();
+        //text(
+        //    [0.7; 4],
+        //    16,
+        //    format!("{}", self.space.time).as_str(),
+        //    self.glyphs,
+        //    self.context.transform.trans(10.0, 20.0),
+        //    self.graphics,
+        //)
+        //.unwrap();
 
-        let ship = &self.space.ships[0];
-        text(
-            [0.7; 4],
-            12,
-            format!("{} thrust: {:?}", ship.name, ship.thrust).as_str(),
-            self.glyphs,
-            self.context.transform.trans(300.0, 20.0),
-            self.graphics,
-        )
-        .unwrap();
+        //let ship = &self.space.ships[0];
+        //text(
+        //    [0.7; 4],
+        //    12,
+        //    format!("{} thrust: {:?}", ship.name, ship.thrust).as_str(),
+        //    self.glyphs,
+        //    self.context.transform.trans(300.0, 20.0),
+        //    self.graphics,
+        //)
+        //.unwrap();
 
-        let selected_body = &self.space.bodies[self.observer.selected_body];
-        text(
-            [0.7; 4],
-            12,
-            format!(
-                "{} position: {:?}",
-                selected_body.name, selected_body.position
-            )
-            .as_str(),
-            self.glyphs,
-            self.context.transform.trans(300.0, 50.0),
-            self.graphics,
-        )
-        .unwrap();
+        //let selected_body = &self.space.bodies[self.observer.selected_body];
+        //text(
+        //    [0.7; 4],
+        //    12,
+        //    format!(
+        //        "{} position: {:?}",
+        //        selected_body.name, selected_body.position
+        //    )
+        //    .as_str(),
+        //    self.glyphs,
+        //    self.context.transform.trans(300.0, 50.0),
+        //    self.graphics,
+        //)
+        //.unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-extern crate piston_window;
 use algebra::Vector;
-use piston_window::*;
 
 mod algebra;
 mod ephemeris;
@@ -8,108 +6,156 @@ mod graphics;
 mod physics;
 mod units;
 
+use amethyst::core::{Transform, TransformBundle};
+use amethyst::input::{InputBundle, StringBindings};
+use amethyst::renderer::Camera;
+use amethyst::ui::{RenderUi, UiBundle};
+use amethyst::window::ScreenDimensions;
 use physics::*;
 
 use crate::graphics::{Frame, Observer};
 use crate::units::Distance;
 
-fn handle_event(event: &Event, observer: &mut graphics::Observer, space: &mut Space) {
-    if let Event::Input(Input::Move(Motion::MouseScroll(zoom_amount)), _) = event {
-        observer.zoom_in_out(zoom_amount[1]);
-    };
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
 
-    // Record position of the mouse to know where the click happened later on.
-    if let Event::Input(Input::Move(Motion::MouseCursor(cursor)), _) = event {
-        observer.mouse_cursor = (cursor[0], cursor[1]);
-    }
+//fn handle_event(event: &Event, observer: &mut graphics::Observer, space: &mut Space) {
+//    if let Event::Input(Input::Move(Motion::MouseScroll(zoom_amount)), _) = event {
+//        observer.zoom_in_out(zoom_amount[1]);
+//    };
+//
+//    // Record position of the mouse to know where the click happened later on.
+//    if let Event::Input(Input::Move(Motion::MouseCursor(cursor)), _) = event {
+//        observer.mouse_cursor = (cursor[0], cursor[1]);
+//    }
+//
+//    // Handle clicks.
+//    if let Event::Input(
+//        Input::Button(ButtonArgs {
+//            state: ButtonState::Press,
+//            button: Button::Mouse(MouseButton::Left),
+//            scancode: _,
+//        }),
+//        _,
+//    ) = event
+//    {
+//        let position = observer.to_world_coords(observer.mouse_cursor);
+//        let body = space.body_at(position);
+//        println!(
+//            "click {:?}, position: {:?}, body: {:?}",
+//            observer.mouse_cursor, position, body
+//        );
+//        if let Some(body) = body {
+//            observer.selected_body = body;
+//        }
+//    };
+//
+//    if let Event::Input(
+//        Input::Button(ButtonArgs {
+//            state: ButtonState::Press,
+//            button: Button::Keyboard(Key::Q),
+//            scancode: _,
+//        }),
+//        _,
+//    ) = event
+//    {
+//        observer.ship_wide_zoom();
+//    };
+//
+//    if let Event::Input(
+//        Input::Button(ButtonArgs {
+//            state: ButtonState::Press,
+//            button: Button::Keyboard(Key::W),
+//            scancode: _,
+//        }),
+//        _,
+//    ) = event
+//    {
+//        observer.system_wide_zoom();
+//    };
+//
+//    if let Event::Input(
+//        Input::Button(ButtonArgs {
+//            state: ButtonState::Press,
+//            button: Button::Keyboard(Key::E),
+//            scancode: _,
+//        }),
+//        _,
+//    ) = event
+//    {
+//        let ship = &mut space.ships[0];
+//        ship.thrust = ship.thrust
+//            + Vector {
+//                x: 0.0,
+//                y: -10.0,
+//                z: 0.0,
+//            };
+//    };
+//
+//    if let Event::Input(
+//        Input::Button(ButtonArgs {
+//            state: ButtonState::Press,
+//            button: Button::Keyboard(Key::R),
+//            scancode: _,
+//        }),
+//        _,
+//    ) = event
+//    {
+//        let ship = &mut space.ships[0];
+//        ship.thrust = Vector {
+//            x: 0.0,
+//            y: 0.0,
+//            z: 0.0,
+//        };
+//    };
+//
+//    if let Event::Loop(Loop::Update(_)) = event {
+//        space.tick(chrono::Duration::hours(1));
+//        observer.look_at(space.bodies[observer.selected_body].position);
+//    }
+//}
 
-    // Handle clicks.
-    if let Event::Input(
-        Input::Button(ButtonArgs {
-            state: ButtonState::Press,
-            button: Button::Mouse(MouseButton::Left),
-            scancode: _,
-        }),
-        _,
-    ) = event
-    {
-        let position = observer.to_world_coords(observer.mouse_cursor);
-        let body = space.body_at(position);
-        println!(
-            "click {:?}, position: {:?}, body: {:?}",
-            observer.mouse_cursor, position, body
-        );
-        if let Some(body) = body {
-            observer.selected_body = body;
-        }
-    };
+struct State;
 
-    if let Event::Input(
-        Input::Button(ButtonArgs {
-            state: ButtonState::Press,
-            button: Button::Keyboard(Key::Q),
-            scancode: _,
-        }),
-        _,
-    ) = event
-    {
-        observer.ship_wide_zoom();
-    };
+impl SimpleState for State {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
 
-    if let Event::Input(
-        Input::Button(ButtonArgs {
-            state: ButtonState::Press,
-            button: Button::Keyboard(Key::W),
-            scancode: _,
-        }),
-        _,
-    ) = event
-    {
-        observer.system_wide_zoom();
-    };
+        // Get the screen dimensions so we can initialize the camera and
+        // place our sprites correctly later. We'll clone this since we'll
+        // pass the world mutably to the following functions.
+        let dimensions = (*world.read_resource::<ScreenDimensions>()).clone();
 
-    if let Event::Input(
-        Input::Button(ButtonArgs {
-            state: ButtonState::Press,
-            button: Button::Keyboard(Key::E),
-            scancode: _,
-        }),
-        _,
-    ) = event
-    {
-        let ship = &mut space.ships[0];
-        ship.thrust = ship.thrust
-            + Vector {
-                x: 0.0,
-                y: -10.0,
-                z: 0.0,
-            };
-    };
+        // Place the camera
+        init_camera(world, &dimensions);
 
-    if let Event::Input(
-        Input::Button(ButtonArgs {
-            state: ButtonState::Press,
-            button: Button::Keyboard(Key::R),
-            scancode: _,
-        }),
-        _,
-    ) = event
-    {
-        let ship = &mut space.ships[0];
-        ship.thrust = Vector {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        };
-    };
+        // Load our sprites and display them
+        //let sprites = load_sprites(world);
+        //init_sprites(world, &sprites, &dimensions);
 
-    if let Event::Loop(Loop::Update(_)) = event {
-        space.tick(chrono::Duration::hours(1));
-        observer.look_at(space.bodies[observer.selected_body].position);
+        //create_ui_example(world);
     }
 }
 
-fn main() {
+fn init_camera(world: &mut World, dimensions: &ScreenDimensions) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(dimensions.width() * 0.5, dimensions.height() * 0.5, 1.);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(dimensions.width(), dimensions.height()))
+        .with(transform)
+        .build();
+}
+
+fn main() -> amethyst::Result<()> {
     let mut space = Space::solar_system();
 
     space.ships.push(Ship {
@@ -125,29 +171,34 @@ fn main() {
 
     println!("Space: {:?}", space);
 
-    let mut window: PistonWindow = WindowSettings::new("Decay", [1280, 720])
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
-    let mut glyphs = window.load_font("./Rajdhani-Light.ttf").unwrap();
-
     let mut observer = Observer::default();
 
-    while let Some(event) = window.next() {
-        handle_event(&event, &mut observer, &mut space);
+    amethyst::start_logger(Default::default());
 
-        window.draw_2d(&event, |context, graphics, device| {
-            let mut frame = Frame {
-                space: &space,
-                observer: &observer,
-                context: &context,
-                graphics: graphics,
-                device: device,
-                glyphs: &mut glyphs,
-            };
+    let app_root = application_root_dir()?;
 
-            frame.draw();
-        });
-    }
+    let resources = app_root.join("assets");
+    let display_config = app_root.join("config/display_config.ron");
+    //let key_bindings_path = app_root.join("config/input.ron");
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        //.with_bundle(
+        //    InputBundle::<StringBindings>::new().with_bindings_from_file(&key_bindings_path)?,
+        //)?
+        //.with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config)?
+                        .with_clear([0.34, 0.36, 0.52, 1.0]),
+                )
+                //.with_plugin(RenderUi::default())
+                .with_plugin(RenderFlat2D::default()),
+        )?;
+
+    let mut game = Application::new(resources, State, game_data)?;
+    game.run();
+
+    Ok(())
 }
