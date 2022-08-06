be086520dafa522a37d926f5c7b0a61f63ed3664
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-08-06 17:53:05 +0200

    draft for movement system

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,18 @@ mod physics;
 mod units;
 
 use physics::*;
+use units::Mass;
 
 use crate::units::Distance;
 
 use bevy::prelude::*;
 
 #[derive(Component)]
-struct Body;
+struct Body {
+    pub position: Vector,
+    pub velocity: Vector,
+    pub mass: Mass,
+}
 
 #[derive(Component)]
 struct Name(String);
@@ -23,9 +28,18 @@ fn create_solar_system(mut commands: Commands) {
     let space = Space::<()>::solar_system(|| ());
 
     for body in space.bodies {
-        commands.spawn().insert(Body).insert(Name(body.name.into()));
+        commands
+            .spawn()
+            .insert(Body {
+                position: body.position,
+                velocity: body.velocity,
+                mass: body.mass,
+            })
+            .insert(Name(body.name.into()));
     }
 }
+
+fn newtownian_movement(query: Query<&Body>) {}
 
 fn main() {
     App::new()
