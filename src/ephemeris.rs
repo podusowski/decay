b5c96bb32bce0424a69e699fc76e9b764026b113
Piotr Podusowski <podusowski@gmail.com>
2022-10-11 11:42:26 +0200

    move camera spawn to its own system

--- src/ephemeris.rs
+++ src/ephemeris.rs
@@ -90,14 +90,4 @@ pub fn spawn_solar_system(
             })
             .insert(Name(body.name.clone()));
     }
-
-    commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, -2.5, 5000000000000.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
-
-    commands.insert_resource(AmbientLight {
-        color: Color::WHITE,
-        brightness: 0.5,
-    });
 }
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,22 @@ mod units;
 
 use bevy::prelude::*;
 
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, -2.5, 5000000000000.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 0.5,
+    });
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_camera)
         .add_startup_system(ephemeris::spawn_solar_system)
         .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
         .add_system(physics::newtonian_gravity)
